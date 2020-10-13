a7c126d91657a583da6a30d789e4ac4a56a56d0b
Marcos Ramalho <marcos.ramalho.1351@gmail.com>
2020-10-13 14:28:59 -0400

    test: add integration with database && move tests users to tests routes users

---
+++ test/routes/user.test.js
@@ -0,0 +1,31 @@
+const request = require('supertest');
+
+const app = require('../../src/app');
+
+test('Deve listar todos os usuários', () => {
+  return request(app)
+    .get('/users')
+    .then((res) => {
+      expect(res.status).toBe(200);
+      expect(res.body.length).toBeGreaterThan(0);
+    });
+});
+
+test('Deve inserir usuário com sucesso', () => {
+  const email = `${Date.now()}@email.com`;
+  const username = `${Date.now()}user`;
+  return request(app)
+    .post('/users')
+    .send({
+      name: 'Marcos',
+      lastName: 'Ramalho',
+      password: '123456',
+      username,
+      email,
+    })
+    .then((res) => {
+      expect(res.status).toBe(201);
+      expect(res.body.name).toBe('Marcos');
+      expect(res.body.lastName).toBe('Ramalho');
+    });
+});

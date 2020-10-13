6f10279c10e0a5ec350782e0cd09a20810f544b8
Marcos Ramalho <marcos.ramalho.1351@gmail.com>
2020-10-13 14:28:32 -0400

    refactor: add integration with database

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,11 +1,30 @@
-module.exports = () => {
+module.exports = (app) => {
   const findAll = (req, res) => {
-    const users = [{ name: 'Marcos', mail: 'marcos@mail.com' }];
-    res.status(200).json(users);
+    app
+      .db('user')
+      .select()
+      .then((result) => {
+        res.status(200).json(result);
+      });
   };
 
-  const create = (req, res) => {
-    res.status(201).json(req.body);
+  const create = async (req, res) => {
+    const data = {
+      use_username: req.body.username,
+      use_email: req.body.email,
+      use_name: req.body.name,
+      use_last_name: req.body.lastName,
+      use_password: req.body.password,
+      cat_createdAt: new Date(),
+    };
+    const result = await app.db('user').insert(data, '*');
+    res.status(201).json({
+      username: result[0].use_username,
+      email: result[0].use_email,
+      lastName: result[0].use_last_name,
+      createdAt: result[0].use_createdAt,
+      name: result[0].use_name,
+    });
   };
 
   return { findAll, create };

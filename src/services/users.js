69672fdea3d553d144328df558a09f56355adc61
Marcos Ramalho <marcos.ramalho.1351@gmail.com>
2020-10-13 15:12:12 -0400

    feat: nao deve inserir usuario com username duplicado

--- src/services/users.js
+++ src/services/users.js
@@ -1,9 +1,9 @@
 module.exports = (app) => {
-  const findAll = () => {
-    return app.db('user').select();
+  const findAll = (filter = {}) => {
+    return app.db('user').where(filter).select();
   };
 
-  const save = (user) => {
+  const save = async (user) => {
     if (!user.use_username) return { error: 'Username é obrigatório' };
     if (!user.use_email) return { error: 'Email é obrigatório' };
     if (!user.use_name) return { error: 'Nome é obrigatório' };
@@ -11,6 +11,10 @@ module.exports = (app) => {
     if (!user.use_password) return { error: 'Senha é obrigatório' };
     if (!user.use_createdAt) return { error: 'Data de criação é obrigatório' };
 
+    const userDB = await findAll({ use_username: user.use_username });
+    if (userDB && userDB.length > 0) {
+      return { error: 'Já existe um usuário com esse username' };
+    }
     return app.db('user').insert(user, '*');
   };
 

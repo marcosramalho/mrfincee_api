8aa160f91ac3dfc0ac880d5f051121aa3a202dfb
Marcos Ramalho <marcos.ramalho.1351@gmail.com>
2020-10-13 16:35:19 -0400

    test: deve inserir uma categoria com sucesso

--- test/routes/categories.test.js
+++ test/routes/categories.test.js
@@ -10,3 +10,17 @@ test('Deve listar todas as categorias', () => {
       expect(res.body.length).toBeGreaterThanOrEqual(0);
     });
 });
+
+test('Deve inserir uma categoria com sucesso', () => {
+  const name = `${Date.now()}cat`;
+  return request(app)
+    .post('/categories')
+    .send({
+      name,
+      description: 'Categoria descrição',
+    })
+    .then((res) => {
+      expect(res.status).toBe(201);
+      expect(res.body.name).toBe(name);
+    });
+});

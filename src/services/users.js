ae352534c35a98b6c3dc9cbd1ada3e272faa1f2c
Marcos Ramalho <marcos.ramalho.1351@gmail.com>
2020-10-13 14:52:38 -0400

    feat: nao deve inserir usuario sem senha

--- src/services/users.js
+++ src/services/users.js
@@ -8,6 +8,7 @@ module.exports = (app) => {
     if (!user.use_email) return { error: 'Email é obrigatório' };
     if (!user.use_name) return { error: 'Nome é obrigatório' };
     if (!user.use_last_name) return { error: 'Sobrenome é obrigatório' };
+    if (!user.use_password) return { error: 'Senha é obrigatório' };
 
     return app.db('user').insert(user, '*');
   };

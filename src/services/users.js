7b6325971903e513b7809e7755088fac6e5c7dd8
Marcos Ramalho <marcos.ramalho.1351@gmail.com>
2020-10-13 14:49:40 -0400

    feat: nao deve inserir usuario sem o nome

--- src/services/users.js
+++ src/services/users.js
@@ -6,6 +6,7 @@ module.exports = (app) => {
   const save = (user) => {
     if (!user.use_username) return { error: 'Username é obrigatório' };
     if (!user.use_email) return { error: 'Email é obrigatório' };
+    if (!user.use_name) return { error: 'Nome é obrigatório' };
 
     return app.db('user').insert(user, '*');
   };

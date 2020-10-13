c6cd9a552209bb4bc56d037ae32d3ec56e806a46
Marcos Ramalho <marcos.ramalho.1351@gmail.com>
2020-10-12 20:26:30 -0400

    feat: deve inserir usuario com sucesso

--- src/app.js
+++ src/app.js
@@ -1,4 +1,8 @@
-const app = require('express')();
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.status(200).send();
@@ -8,6 +12,10 @@ app.get('/users', (req, res) => {
   const users = [{ name: 'Marcos', mail: 'marcos@mail.com' }];
 
   res.status(200).json(users);
+});
+
+app.post('/users', (req, res) => {
+  res.status(201).json(req.body);
 });
 
 module.exports = app;

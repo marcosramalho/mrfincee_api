65fd2cdb468605e8accd33d86953c5e34afb9bf0
Marcos Ramalho <marcos.ramalho.1351@gmail.com>
2020-10-12 20:26:39 -0400

    test: deve inserir usuario com sucesso

--- test/user.test.js
+++ test/user.test.js
@@ -11,3 +11,13 @@ test('Deve listar todos os usuários', () => {
       expect(res.body[0]).toHaveProperty('name', 'Marcos');
     });
 });
+
+test('Deve inserir usuário com sucesso', () => {
+  return request(app)
+    .post('/users')
+    .send({ name: 'Marcos Ramalho', mail: 'marcosr@mail.com' })
+    .then((res) => {
+      expect(res.status).toBe(201);
+      expect(res.body.name).toBe('Marcos Ramalho');
+    });
+});

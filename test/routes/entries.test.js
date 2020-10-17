384db1588d9df9ce756cc8d9a69fd9c3dc7c5b9f
Marcos Ramalho <marcos.ramalho.1351@gmail.com>
2020-10-17 19:51:45 -0400

    test: deve inserir um lancamento com sucesso

---
+++ test/routes/entries.test.js
@@ -0,0 +1,58 @@
+const request = require('supertest');
+
+const app = require('../../src/app');
+
+// expense --> despesa
+// revenue --> receita
+
+const MAIN_ROUTE = '/entries';
+let user;
+let category;
+
+beforeAll(async () => {
+  const [resUser] = await app.services.users.save({
+    use_username: `${Date.now()}user`,
+    use_email: `${Date.now()}@email.com`,
+    use_name: 'User',
+    use_last_name: 'Entry',
+    use_password: '123456',
+    use_createdAt: new Date(),
+  });
+
+  const [resCategory] = await app.services.categories.save({
+    cat_name: `${Date.now()}cat`,
+    cat_description: 'Categoria descrição',
+    cat_createdAt: new Date(),
+  });
+
+  user = {
+    id: resUser.use_id,
+    name: resUser.use_name,
+    lastName: resUser.use_last_name,
+    username: resUser.use_username,
+    email: resUser.use_email,
+  };
+
+  category = {
+    id: resCategory.cat_id,
+    name: resCategory.cat_name,
+    description: resCategory.cat_description,
+  };
+});
+
+test('Deve inserir um lançamento com sucesso', () => {
+  return request(app)
+    .post(MAIN_ROUTE)
+    .send({
+      name: 'Lanc #1',
+      amount: 10,
+      type: 'expense',
+      description: 'Lanc #1',
+      userId: user.id,
+      categoryId: category.id,
+    })
+    .then((result) => {
+      expect(result.status).toBe(201);
+      expect(result.body.name).toBe('Lanc #1');
+    });
+});

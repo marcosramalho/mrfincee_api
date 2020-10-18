6f36a0bbff91d51ad99b3403311adc575e57a7ae
Marcos Ramalho <marcos.ramalho.1351@gmail.com>
2020-10-17 20:31:25 -0400

    feat: nao deve inserir lancamento sem valor

--- src/services/entries.js
+++ src/services/entries.js
@@ -1,6 +1,7 @@
 module.exports = (app) => {
   const create = (entry) => {
     if (!entry.ent_name) return { error: 'Nome é obrigatório' };
+    if (!entry.ent_amount) return { error: 'Valor é obrigatório' };
 
     return app.db('entry').insert(entry, '*');
   };
